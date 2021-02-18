a22bae1b1cdfcddc18cd3495b3a4ed7fadbde33c
PawelGajewski <pawel.gajewski1.9@gmail.com>
2021-02-18 17:28:44 +0100

    add: initialCommit

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,19 @@
+import Task from "./Task";
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <div className="tasks">
+        <span>Twoje Zadania</span>
+      {tasks.map((task) => (
+        <Task 
+        key={task.id} 
+        task={task} 
+        onDelete={onDelete} 
+        onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Tasks;
